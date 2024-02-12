ada957a8907c1f905e21fe69f80f33c933052cc7
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-02-12 09:23:41 +0100

    Configure setup modules, devtool + plugin
    Feat: added HTMLWebpackPlugin to use custom HTML templates
    Feat: added CSS + image modules so CSS and images can be imported
    using webpack
    Feat: added inline source map as devtool so browser debugging points
    to right file (not generic main.js)
    Feat: added webpack-dev-server for auto reloading of pages on save

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./src",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  optimization: {
+    runtimeChunk: "single",
+  },
+};
