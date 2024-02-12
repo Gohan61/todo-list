4be6a70406eaba2ce4e78aabe5c4140c3959f2f2
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-02-12 09:28:34 +0100

    Create directories dist+src + files
    Feat: create directories dist and src for use with webpack. Loading
    all files from src to compile in dist.
    Feat: use src/index.html as template file with HTMLWebpackPlugin
    Feat: use src/index.js as main js file for export to main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+(self["webpackChunktodo_list"] = self["webpackChunktodo_list"] || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log("Testing");
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kby1saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiVGVzdGluZ1wiKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
