d9b16b79dfa157b543bb1b26ad8a4ba4e1d2b9e8
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-02-21 16:49:04 +0100

    Style page + lists & to-do's

--- src/index.js
+++ src/index.js
@@ -39,27 +39,5 @@ const newListObj = newList(
   deleteListObj
 );
 
-// const currentListObj = currentList(listObj, newToDoObj.id);
-
-// listObj.addLists("second");
-
-// toDoObj.addToDo("Tasks", "First to do", "Here we go", "2024", "!");
-// // toDoObj.addToDo("Tasks", "Some more to do on first");
-// // toDoObj.addToDo("Tasks", "Try third");
-// toDoObj.addToDo("second", "Second to do", "Trying", 2025, "", 0);
-
-// // completed.checkCompleted(0);
-// completed.checkCompleted(0, "second");
-
-// completed.undoComplete(0, "second");
-// //
-
-// priorityObj.changePriority(0, "second");
-
-// // deleteObj.deleteToDo(0);
-// // deleteObj.deleteList("second");
-
-// toDoObj.editToDo(0, "title", "Changed second", "second");
-
 console.log(completed.getCompletedList());
 console.log(listObj.getLists());
