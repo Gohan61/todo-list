76013692c5e136ec3b1a43f711bc29105d6fbda3
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-04-08 17:20:35 +0200

    Fix: delete lists from completed as well

--- src/deleteLists.js
+++ src/deleteLists.js
@@ -1,5 +1,9 @@
 import { listObj } from "./list";
+import { completedListObj } from "./completeToDo";
 
-export default function deleteLists(listObject, listName) {
-  delete listObject.getLists()[listName];
+export default function deleteLists(listName) {
+  delete listObj.getLists()[listName];
+  if (Object.hasOwn(completedListObj.getCompletedLists(), listName)) {
+    delete completedListObj.getCompletedLists()[listName];
+  }
 }
