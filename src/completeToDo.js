a66967d58ddafbca8fb95abf216f1c1693355b7a
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-04-08 16:49:56 +0200

    Fix: update retrieving from localStorage + setting
    * Set localStorage using set method so get method stays available in
    function

--- src/completeToDo.js
+++ src/completeToDo.js
@@ -1,22 +1,46 @@
-export default function completeToDo(listObj) {
-  const completedList = {};
+// export default function completeToDo(listObj) {
+//   const completedList = {};
 
-  function checkCompleted(id, list = "Tasks") {
-    if (Object.hasOwn(completedList, list)) {
-    } else {
-      completedList[list] = {};
-    }
-    completedList[list][id] = listObj.getLists()[list][id];
-    console.log(completedList);
+//   function checkCompleted(id, list = "Tasks") {
+//     if (Object.hasOwn(completedList, list)) {
+//     } else {
+//       completedList[list] = {};
+//     }
+//     completedList[list][id] = listObj.getLists()[list][id];
+//     console.log(completedList);
 
-    delete listObj.getLists()[list][id];
-  }
+//     delete listObj.getLists()[list][id];
+//   }
 
-  const getCompletedList = () => completedList;
+//   const getCompletedList = () => completedList;
 
-  function undoComplete(id, list = "Tasks") {
-    listObj.getLists()[list][id] = completedList[list][id];
-    delete completedList[list][id];
-  }
-  return { checkCompleted, getCompletedList, undoComplete };
+//   function undoComplete(id, list = "Tasks") {
+//     listObj.getLists()[list][id] = completedList[list][id];
+//     delete completedList[list][id];
+//   }
+//   return { checkCompleted, getCompletedList, undoComplete };
+// }
+
+export default function completedList() {
+  const completedLists = {
+    Tasks: {},
+  };
+
+  const getCompletedLists = () => completedLists;
+  const setCompletedLists = (localList) => (completedList = localList);
+
+  return { getCompletedLists, setCompletedLists };
 }
+
+export let completedListObj;
+if (JSON.parse(localStorage.getItem("completedTasks"))) {
+  completedListObj.setCompletedLists(
+    JSON.parse(localStorage.getItem("completedTasks")),
+  );
+} else {
+  completedListObj = completedList();
+}
+
+// function checkCompleted(id, list) {
+//   if (Object.hasOwn(completedListObj))
+// }
