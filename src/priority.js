66f8ddffe278d363801cb341d6a972cade27a6c3
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-04-11 15:23:36 +0200

    Feat: save priority changes to local storage

--- src/priority.js
+++ src/priority.js
@@ -1,9 +1,12 @@
 import { listObj } from "./list";
+import storeLocal from "./localStorage";
 
-function changePriority(id, list) {
+export default function changePriority(id, list) {
   if (listObj.getLists()[list][id].priority === false) {
     listObj.getLists()[list][id].priority = true;
+    storeLocal(listObj.getLists());
   } else {
     listObj.getLists()[list][id].priority = false;
+    storeLocal(listObj.getLists());
   }
 }
